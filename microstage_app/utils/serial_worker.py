5ac9139d6813b7c6f6513ad6b33e524a5b631804
GerryDoesStuff <107370706+GerryDoesStuff@users.noreply.github.com>
2025-08-24 21:50:37 +0300

    Fix ROI centering by tracking sensor size

--- microstage_app/utils/serial_worker.py
+++ microstage_app/utils/serial_worker.py
@@ -4,6 +4,7 @@ from queue import Queue, Empty
 class SerialWorker(QtCore.QObject):
     finished = QtCore.Signal()
     errored = QtCore.Signal(str)
+    result = QtCore.Signal(object, object)  # callback, result
 
     def __init__(self, stage):
         super().__init__()
@@ -16,18 +17,20 @@ class SerialWorker(QtCore.QObject):
         try:
             while self._running:
                 try:
-                    fn, args, kwargs = self._q.get(timeout=0.1)
+                    fn, args, kwargs, cb = self._q.get(timeout=0.1)
                 except Empty:
                     continue
                 try:
-                    fn(*args, **kwargs)
+                    res = fn(*args, **kwargs)
+                    if cb:
+                        self.result.emit(cb, res)
                 except Exception as e:
                     self.errored.emit(str(e))
         finally:
             self.finished.emit()
 
-    def enqueue(self, fn, *args, **kwargs):
-        self._q.put((fn, args, kwargs))
+    def enqueue(self, fn, *args, callback=None, **kwargs):
+        self._q.put((fn, args, kwargs, callback))
 
     def stop(self):
         self._running = False
