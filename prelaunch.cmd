cf20d13c09d80b49a676c04c30f1e90ed3dc27ca
GerryDoesStuff <usetheshovel@gmail.com>
2025-09-21 14:37:59 +0300

    Prelaunch adjust

--- prelaunch.cmd
+++ prelaunch.cmd
@@ -1,4 +1,2 @@
 cd C:\Users\HS00019\OneDrive\Documents\Apps\MicroTableController\MicroTableController_v3\microstage_app
 .\scripts\activate.bat
-cd C:\Users\HS00019\OneDrive\Documents\Apps\MicroTableController\MicroTableController_v3
-python -m microstage_app
---
+++ prelaunch2.cmd
@@ -0,0 +1,2 @@
+cd C:\Users\HS00019\OneDrive\Documents\Apps\MicroTableController\MicroTableController_v3
+python -m microstage_app
